c2d2c7eebdd8cecc9595f6fedab04fc307d61872
LorenzoTappi <tappi.loren@gmail.com>
2022-03-19 21:04:59 -0300

    agregue navBar

---
+++ carpeta-madre/app31855/src/components/navBar.js
@@ -0,0 +1,20 @@
+const navBar = () => {
+    return (
+
+       /*  <header>
+            <h1>MiTienda</h1>
+            <img src={} />
+        </header>
+ */
+        <nav>
+
+            <button>Home</button>
+            <button>Productos</button>
+            <button>Contacto</button>
+            <button>Instagram</button>
+            <button>Cambio-Devolucion</button>
+
+        </nav>
+    )
+}
+export default navBar
