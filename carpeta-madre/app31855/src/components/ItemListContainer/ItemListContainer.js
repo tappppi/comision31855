9f235715eee87c057c63da7ca694cc8b778165a7
LorenzoTappi <tappi.loren@gmail.com>
2022-03-30 20:12:30 -0300

    añadiendo asynmock y productos

--- carpeta-madre/app31855/src/components/ItemListContainer/ItemListContainer.js
+++ carpeta-madre/app31855/src/components/ItemListContainer/ItemListContainer.js
@@ -1,8 +1,29 @@
+import { getProduct } from "../../asymock"
+import { useState, useEffect} from "react"
+import Itemlist from "../ItemList"
 const ItemListContainer = ({greeting}) => {
 
-    return(
-        <h1>{greeting}</h1>
+    const [prodcuts, setProducts] = useState([])
 
+
+    useEffect(()=>{
+
+        getProduct().then(Response => {
+            setProducts(Response)
+        })
+
+    }, [])
+
+    console.log(setProducts)
+    return(
+        <div>
+            <h1>{greeting}</h1>
+
+            <ul>
+                {products.map(product => <li key={product.id} > { product.name }</li>)}
+            </ul>
+            <Itemlist products = {products} />
+        </div>
     )
 
 }
