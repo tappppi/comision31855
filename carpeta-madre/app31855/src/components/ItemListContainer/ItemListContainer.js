5a972cab507594eb3992587131237ba8f7a3de80
LorenzoTappi <tappi.loren@gmail.com>
2022-04-02 20:37:23 -0300

    aprendiendo fetch

--- carpeta-madre/app31855/src/components/ItemListContainer/ItemListContainer.js
+++ carpeta-madre/app31855/src/components/ItemListContainer/ItemListContainer.js
@@ -4,23 +4,50 @@ import Itemlist from "../ItemList"
 const ItemListContainer = ({greeting}) => {
 
     const [prodcuts, setProducts] = useState([])
+    const [search, setSearch] = useState("")
 
 
-    useEffect(()=>{
+
+
+    /* useEffect(() =>{
+
+       fetch("https://api.mercadolibre.com/sites/MLA/search?q=notebook")
+       .then(response => {
+           console.log(response)
+       }).then(res => {
+           setProducts(res.results)
+       })
+       
+
+
+    }, [])
+ */
+    const fetchProducts = () => {
+        fetch("https://api.mercadolibre.com/sites/MLA/search?q=" + search)
+        .then(response => {
+            return response.json()
+        }).then(res => {
+            setProducts(res.results)
+        })
+
+    }
+  /*   useEffect(()=>{
 
         getProduct().then(Response => {
             setProducts(Response)
         })
 
     }, [])
-
+ */
     console.log(setProducts)
     return(
         <div>
             <h1>{greeting}</h1>
+            <input type="text" onChange={(e) => setSearch(e.target.value)}> </input>
+            <button onClick={fetchProducts}>Buscar</button>
 
             <ul>
-                {products.map(product => <li key={product.id} > { product.name }</li>)}
+                {products.map(product => <img key={product.id} src={product.tumbnail} alt= "img"></img>)}
             </ul>
             <Itemlist products = {products} />
         </div>
